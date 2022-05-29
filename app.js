0a47890aab7107edf88f6b4196074f2b791e7697
yijun <sandy3068@yahoo.com.tw>
2022-05-30 01:09:57 +0800

    修改錯誤訊息

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+const errHandle = require('./handle/errHandle');
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 
@@ -35,20 +36,13 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  // res.status(404).send('Sorry cant find that!');
+  errHandle(res, 404);
 });
-
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  console.error(err.stack);
+  errHandle(res, 500);
 });
 
 module.exports = app;
--- handle/errHandle.js
+++ handle/errHandle.js
@@ -1,7 +1,15 @@
 const errHandle = (res, code) => {
+  let message;
+  if (code === 404) {
+    message = '找不到此路由';
+  } else if (code === 400) {
+    message = '欄位未填寫正確，或無此 id';
+  } else if (code === 500) {
+    message = '500錯誤';
+  }
   res.status(code).json({
     'status': 'false',
-    'message': '欄位未填寫正確，或無此 id',
+    'message': message,
   })
 }
 module.exports = errHandle;
